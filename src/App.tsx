ce5da7956d03118eaa07e662c91b6feda1343eda
0603anton <zorgzb@gmail.com>
2022-09-14 18:01:08 +0200

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello, samurai! Let's go!</h1>
+            <Header/>
+        </div>
+    );
 }
 
 export default App;
