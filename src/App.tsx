433372a8f51f749f4bfc8cd1983323e7d9b6ab87
0603anton <zorgzb@gmail.com>
2022-09-16 14:32:03 +0200

    lesson 10 - done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
+import {Technologies} from "./components/Technologies/Technologies";
+
 
 function App() {
     return (
         <div className="App">
             <h1>Hello, samurai! Let's go!</h1>
             <Header/>
+            <Technologies/>
         </div>
     );
 }
+
 
 export default App;
