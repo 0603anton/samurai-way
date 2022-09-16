78a9787ca4b9d45a4b188d5b77b687ac6844ac82
0603anton <zorgzb@gmail.com>
2022-09-16 21:17:37 +0200

    lesson 11 - done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
-import {Technologies} from "./components/Technologies/Technologies";
 
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello, samurai! Let's go!</h1>
-            <Header/>
-            <Technologies/>
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGvUYDg2lhrLe-eeprr-c51m0Kv7wR4tw06w&usqp=CAU"
+                    alt=""/>
+            </header>
+            <nav className="nav">
+                <ul>
+                    <li>Profile</li>
+                    <li>Message</li>
+                    <li>News</li>
+                    <li>Music</li>
+                    <li>Settings</li>
+                </ul>
+            </nav>
+            <div className="content">
+                <div>
+                    <img className="content-img" src="https://wallpaperaccess.com/full/1131217.jpg" alt=""/>
+                </div>
+                <div>
+                    ava+description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
