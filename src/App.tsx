75ba54fa37c1c0bd8263d766eb9eb5596ba314e6
0603anton <zorgzb@gmail.com>
2022-09-18 20:30:19 +0200

    lesson 14 - done

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 
 function App() {
     return (
         <div className="app-wrapper">
-            <header className="header">
-                <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGvUYDg2lhrLe-eeprr-c51m0Kv7wR4tw06w&usqp=CAU"
-                    alt=""/>
-            </header>
-            <nav className="nav">
-                <ul>
-                    <li>Profile</li>
-                    <li>Message</li>
-                    <li>News</li>
-                    <li>Music</li>
-                    <li>Settings</li>
-                </ul>
-            </nav>
-            <div className="content">
-                <div>
-                    <img className="content-img" src="https://wallpaperaccess.com/full/1131217.jpg" alt=""/>
-                </div>
-                <div>
-                    ava+description
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        post1
-                    </div>
-                    <div>
-                        post 2
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import _ from "./Header.module.css"
+
+export const Header = () => {
+    return (
+        <header className={_.header}>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGvUYDg2lhrLe-eeprr-c51m0Kv7wR4tw06w&usqp=CAU"
+                alt=""/>
+        </header>
+    );
+};
+
