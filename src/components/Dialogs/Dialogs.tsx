14d9ede3ac0e9a2fb30f2f2f17d129ea7e4050bb
0603anton <zorgzb@gmail.com>
2022-10-04 10:59:16 +0200

    lesson 21 - done

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,7 +4,23 @@ import _ from './Dialogs.module.css'
 export const Dialogs = () => {
     return (
         <div>
-            Dialogs
+            <div className={_.dialogs}>
+                <div className={_.companions}>
+                    <div className={_.companion}>companion1</div>
+                    <div className={_.companion}>companion2</div>
+                    <div className={_.companion}>companion3</div>
+                    <div className={_.companion}>companion4</div>
+                    <div className={_.companion}>companion5</div>
+                    <div className={_.companion}>companion6</div>
+                </div>
+                <div className={_.chats}>
+                    <div className={_.message}>hi</div>
+                    <div className={_.message}>how are you
+                        ?
+                    </div>
+                    <div className={_.message}>Yo</div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,11 +6,11 @@ export const Navbar = () => {
     return (
         <nav className={_.nav}>
             <ul>
-                <li className={`${_.item} ${_.active}`}><NavLink to='/profile'>Profile</NavLink></li>
-                <li className={_.item}><NavLink to={'/dialogs'}>Messages</NavLink></li>
-                <li className={_.item}><NavLink to={'/news'}>News</NavLink></li>
-                <li className={_.item}><NavLink to={'/music'}>Music</NavLink></li>
-                <li className={_.item}><NavLink to={'/settings'}>Settings</NavLink></li>
+                <li className={_.item}><NavLink to='/profile' activeClassName={_.selected}>Profile</NavLink></li>
+                <li className={_.item}><NavLink to={'/dialogs'} activeClassName={_.selected}>Messages</NavLink></li>
+                <li className={_.item}><NavLink to={'/news'} activeClassName={_.selected}>News</NavLink></li>
+                <li className={_.item}><NavLink to={'/music'} activeClassName={_.selected}>Music</NavLink></li>
+                <li className={_.item}><NavLink to={'/settings'} activeClassName={_.selected}>Settings</NavLink></li>
             </ul>
         </nav>
     );
