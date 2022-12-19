569ac333c33f0905845fe83992dbae355feae27a
0603anton <zorgzb@gmail.com>
2022-12-19 23:51:20 +0200

    lesson 44 Context API - done

--- src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -4,34 +4,43 @@ import {ActionType} from '../../../State/state';
 import {addPostActionCreator, updateNewPostTextAC} from '../../../State/profile-reducer';
 import MyPosts from "./MyPosts";
 import {StateRootType} from "../../../index";
+import StoreContext from "../../../StoreContext";
 
-type MyPostsContainerPropsType={
+type MyPostsContainerPropsType = {
     // posts: PostType[]
     // newPostText: string
     store: StateRootType
-dispatch: (action: ActionType) => void
+    dispatch: (action: ActionType) => void
 }
 
 
 const MyPostsContainer = (props: MyPostsContainerPropsType) => {
 
-    const onButtonClickAddPostHandler = () => {
-        // props.addPost()
-        props.dispatch(addPostActionCreator())
-    }
 
-    function onCnahgeTextAreaHandler(text:string) {
-        let action = updateNewPostTextAC(text)
-        props.dispatch(action)
-    }
+
 
 
     return (
- <MyPosts
-     posts={props.store.profilePage.posts}
-     updateNewPostText={onCnahgeTextAreaHandler}
-     addPost={onButtonClickAddPostHandler}
-     newPostText={props.store.profilePage.newPostText} />
+        <StoreContext.Consumer>
+            {(store) => {
+                const onButtonClickAddPostHandler = () => {
+                    // props.addPost()
+                    store.dispatch(addPostActionCreator())
+                }
+
+                function onCnahgeTextAreaHandler(text: string) {
+                    let action = updateNewPostTextAC(text)
+                    store.dispatch(action)
+                }
+
+               return <MyPosts
+                    posts={store.getState().profilePage.posts} // тут данные уже не через пропс берутся а через контекст
+                    updateNewPostText={onCnahgeTextAreaHandler}
+                    addPost={onButtonClickAddPostHandler}
+                    newPostText={store.getState().profilePage.newPostText}// тут данные уже не через пропс берутся а через контекст
+                />}
+            }
+        </StoreContext.Consumer>
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,19 @@ import App from './App';
 // import {RootStateType, store} from './State/state'; // тут из редакс стора
 import {BrowserRouter} from "react-router-dom";
 import store from "./State/redux-store";
+import StoreContext from "./StoreContext";
+import storeContext from "./StoreContext";
 
 
 export type StateRootType = ReturnType<typeof store.getState>
 let rerenderEntireTree = (state: StateRootType) => { // здесь добавили автотипизацию
     ReactDOM.render(
         <BrowserRouter>
+            <StoreContext.Provider value={store}>
             <App state={state}
                  dispatch={store.dispatch.bind(store)}  // забиндили зафиксировали начальный объект, к чему будет обращаться this
                  />
+            </StoreContext.Provider>
         </BrowserRouter>, // т.к. далее не будет работать т.к. грубо говоря роходя по пропсам глубже this будет срабатывать на props
         document.getElementById('root') // из-за этого была ошибка this._callSubscriber is not a function - без binda ссылалось на другой объект
     );
